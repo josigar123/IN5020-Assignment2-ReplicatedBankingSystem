3a94ce721a9aeca492e2f0e6d8ea82a0349ad4d8
Kacpers03 <114741654+Kacpers03@users.noreply.github.com>
2025-09-25 22:07:30 +0200

    enkelt rmi interface for bankservice

--- common/src/main/java/BankService.java
+++ common/src/main/java/BankService.java
@@ -1,7 +1,10 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 // RMI method interface for banking replication consistency
 public interface BankService extends Remote {
+    void deliverOrderedBatch(String groupName, long sequence, List<Transaction> transactions) throws RemoteException;
+    void onMembershipChange(String groupName, List<String> currentReplicaIds) throws RemoteException;
 
 }
