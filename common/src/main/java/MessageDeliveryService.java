7a63f34e7d2bad394498c1fccf7ce155f588e9c9
josigar <104169012+josigar123@users.noreply.github.com>
2025-09-28 16:23:46 +0200

    Change return type

--- common/src/main/java/MessageDeliveryService.java
+++ common/src/main/java/MessageDeliveryService.java
@@ -1,11 +1,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
-import java.util.OptionalDouble;
+import java.util.Map;
 
 // RMI methods for interacting with the MDS
 public interface MessageDeliveryService extends Remote {
-    OptionalDouble joinGroup(String groupName, String bankServer) throws RemoteException;
+    Map<String, Double> joinGroup(String groupName, String bankServer) throws RemoteException;
     void leaveGroup(String groupName, String bankServer) throws RemoteException;
     void sendTransactions(String groupName, List<Transaction> transactions) throws RemoteException;
     int getNumberOfMembers(String groupName) throws RemoteException;
