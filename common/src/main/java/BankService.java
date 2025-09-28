72d1809321fdda384d4b1d451c5333b56d6ad4ad
josigar <104169012+josigar123@users.noreply.github.com>
2025-09-28 16:25:10 +0200

    Refactor interface for getBalance();
    Now returns a map of all currencies and their balance at each bank replica so that it can be assigned to later banks

--- common/src/main/java/BankService.java
+++ common/src/main/java/BankService.java
@@ -1,11 +1,12 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 
 // RMI method interface for banking replication consistency
 public interface BankService extends Remote {
     boolean deliverOrderedBatch(List<OrderedTransaction> orderedTransactions) throws RemoteException;
     void onMembershipChange(String groupName, List<String> currentReplicaIds) throws RemoteException;
-    double getBalance(String groupName, String currency) throws RemoteException;
+    Map<String, Double> getBalance() throws RemoteException;
 
 }
