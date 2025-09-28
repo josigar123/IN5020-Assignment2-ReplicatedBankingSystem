0a8b195b8875ab4b5a28c81d094b4e28e2b48f63
josigar <104169012+josigar123@users.noreply.github.com>
2025-09-28 15:13:11 +0200

    Change return type of join group

--- common/src/main/java/MessageDeliveryService.java
+++ common/src/main/java/MessageDeliveryService.java
@@ -1,12 +1,12 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
-import java.util.OptionalInt;
+import java.util.OptionalDouble;
 
 // RMI methods for interacting with the MDS
 public interface MessageDeliveryService extends Remote {
-    OptionalInt joinGroup(String groupName, String bankServer) throws RemoteException;
+    OptionalDouble joinGroup(String groupName, String bankServer) throws RemoteException;
     void leaveGroup(String groupName, String bankServer) throws RemoteException;
-    void sendTransactions(List<Transaction> transactions) throws RemoteException;
+    void sendTransactions(String groupName, List<Transaction> transactions) throws RemoteException;
     int getNumberOfMembers(String groupName) throws RemoteException;
 }
