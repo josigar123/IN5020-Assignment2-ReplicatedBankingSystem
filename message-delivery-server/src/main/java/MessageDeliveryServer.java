33fa4595f291c23fd9506d7b5f0f4b37e73034e6
josigar <104169012+josigar123@users.noreply.github.com>
2025-09-28 15:12:02 +0200

    Create a coordinator map to pass to MDSImpl
    MDS can no coordinate on multiple groups, accessed via their group name

--- message-delivery-server/src/main/java/MessageDeliveryServer.java
+++ message-delivery-server/src/main/java/MessageDeliveryServer.java
@@ -1,6 +1,7 @@
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MessageDeliveryServer {
 
@@ -17,15 +18,21 @@ public class MessageDeliveryServer {
         Registry registry = LocateRegistry.getRegistry(REGISTRY_IP, REGISTRY_PORT);
         System.out.println("[MDS] Located registry");
 
-        System.out.println("[MDS] Creating registry");
+        System.out.println("[MDS] Creating coordinator");
         TransactionCoordinator coordinator = new TransactionCoordinator(group);
+        // Concurrent hashmap of coordinators to pass to MDSImpl
+        ConcurrentHashMap<String, TransactionCoordinator> coordinators = new ConcurrentHashMap<>();
+        // Add the one group to the map
+        coordinators.put(coordinator.getGroup().getGroupName(), coordinator);
 
-        // Create the service for registration with the group
-        MessageDeliveryServiceImpl service = new MessageDeliveryServiceImpl(coordinator);
+        // Pass the map of coordinators, the MDSImpl, can now coordinate for multiple groups, each with their
+        // own coordinator
+        MessageDeliveryServiceImpl service = new MessageDeliveryServiceImpl(coordinators);
 
         // bind service to registry
         System.out.println("[MDS] Creating service");
         registry.rebind("message-delivery-service", service);
+        System.out.println("[MDS] Bound service");
 
         System.out.println("[MDS] Message delivery server started");
 
