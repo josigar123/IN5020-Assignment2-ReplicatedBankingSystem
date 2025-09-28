4c6ac8fb8437014f1c1bfcc742b45bdd287c4a24
josigar <104169012+josigar123@users.noreply.github.com>
2025-09-28 15:55:33 +0200

    Add RMI interface for getting members by name

--- common/src/main/java/MessageDeliveryService.java
+++ common/src/main/java/MessageDeliveryService.java
@@ -9,4 +9,5 @@ public interface MessageDeliveryService extends Remote {
     void leaveGroup(String groupName, String bankServer) throws RemoteException;
     void sendTransactions(String groupName, List<Transaction> transactions) throws RemoteException;
     int getNumberOfMembers(String groupName) throws RemoteException;
+    List<String> getMemberNames(String groupName) throws RemoteException;
 }
