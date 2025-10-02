3a394dbdcf236e684460e81643f0d37191b76f20
josigar <104169012+josigar123@users.noreply.github.com>
2025-10-02 20:15:17 +0200

    Add executor thread for received view.

--- bank-server/src/main/java/BankServiceImpl.java
+++ bank-server/src/main/java/BankServiceImpl.java
@@ -2,13 +2,15 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BankServiceImpl extends UnicastRemoteObject implements BankService {
 
     private final String groupName;
-    private final Map<String, Double> balanceByCurrency = new ConcurrentHashMap<>();
-    private BankRepository repository;
+    private final BankRepository repository;
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public BankServiceImpl(String groupName, BankRepository repository) throws RemoteException {
         this.groupName = groupName;
@@ -23,7 +25,39 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
             repository.addOutstandingTransaction(transaction);
         }
 
-        return true;
+        executor.submit(()->{
+           int selfCounter = 0;
+           CommandParser parser = new CommandParser(repository);
+           for(Transaction transaction : repository.getOutstandingTransactions()){
+               boolean skipExecution = false;
+               String prefix = transaction.getUniqueId().split(":")[0];
+               // Filter stale transactions
+               for(Transaction executedTransaction : repository.getExecutedTransactions()){
+                   if(executedTransaction.getUniqueId().equals(transaction.getUniqueId())){
+                       repository.getOutstandingTransactions().remove(executedTransaction);
+                       skipExecution = true;
+                   }
+               }
+
+               if(skipExecution){
+                   continue;
+               }
+
+               if(repository.getBankBindingName().equals(prefix) && selfCounter == repository.getSelfTransactions()){
+                   continue;
+               }
+
+               if(repository.getBankBindingName().equals(prefix) && selfCounter < repository.getSelfTransactions()){
+                  selfCounter++;
+               }
+
+               // This method appends the transaction to the executed list, and also remove from outstanding collections
+               parser.executeTransaction(transaction.command());
+
+           }
+        });
+
+        return true; // ACK
     }
 
     @Override
@@ -31,18 +65,14 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
         throw  new UnsupportedOperationException("Not supported yet.");
     }
 
+    // TODO: Implement this so that new banks can get the correct view
+    //       MDS iterates over all group members, fetching their balance until the first correct return
     @Override
     public Map<String, Double> getBalance() throws RemoteException {
-        return Map.copyOf(balanceByCurrency);
+        throw  new UnsupportedOperationException("Not supported yet.");
     }
 
 
-    public void setInitialBalance(Map<String, Double> initialBalance) throws RemoteException {
-      balanceByCurrency.clear();
-      balanceByCurrency.putAll(initialBalance);
-    }
-
-    
     public BankRepository getRepository() {
         return this.repository;
 }
