5293d0badcd0e332cffe51ab523483e27b549a1e
josigar <104169012+josigar123@users.noreply.github.com>
2025-10-03 16:52:28 +0200

    Change script

--- runMds.sh
+++ runMds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp "message-delivery-server/target/message-delivery-server-1.0-SNAPSHOT.jar:common/target/common-1.0-SNAPSHOT.jar:bank-server/target/bank-server-1.0-SNAPSHOT.jar" MessageDeliveryServer $1
+java -cp "message-delivery-server/target/message-delivery-server-1.0-SNAPSHOT.jar:common/target/common-1.0-SNAPSHOT.jar:bank-server/target/bank-server-1.0-SNAPSHOT.jar" MessageDeliveryServer
 
