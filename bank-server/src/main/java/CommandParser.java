44f14fbb3b36a1446d636349ca21bd96fe449d60
josigar <104169012+josigar123@users.noreply.github.com>
2025-10-06 16:24:11 +0200

    Improve parsing

--- bank-server/src/main/java/CommandParser.java
+++ bank-server/src/main/java/CommandParser.java
@@ -16,9 +16,71 @@ public class CommandParser {
     }
 
     public void buildOutstandingTransactions(String command){
-        String uniqueId = repository.getBankBindingName()+ ":" + repository.getOutstandingCount();
-        Transaction tx = new Transaction(command, uniqueId);
-        repository.addOutstandingTransaction(tx);
+        boolean isValid = verifyTransaction(command);
+
+        if(isValid) {
+            String uniqueId = repository.getBankBindingName() + ":" + repository.getOutstandingCount();
+            Transaction tx = new Transaction(command, uniqueId);
+            repository.addOutstandingTransaction(tx);
+        }
+    }
+
+    public boolean verifyTransaction(String input){
+        String[] tokens = parseTransactionFromLine(input);
+        String cmd = tokens[0];
+
+        try {
+            switch (cmd.toLowerCase()) {
+                case "getquickbalance":
+                    if (tokens.length < 2) {
+                        System.out.println("[BANK] [ERROR] Usage: getQuickBalance <currency>");
+                        break;
+                    }
+                    return true;
+
+                case "getsyncedbalance":
+                    if (tokens.length < 2) {
+                        System.out.println("[BANK] [ERROR] Usage: getSyncedBalance <currency>");
+                        break;
+                    }
+                    return true;
+
+                case "deposit":
+                    if (tokens.length < 3) {
+                        System.out.println("[BANK] [ERROR] Usage: deposit <currency> <amount>");
+                        break;
+                    }
+                    return true;
+
+                case "addinterest":
+                    if (tokens.length < 3) {
+                        System.out.println("[BANK] [ERROR] Usage: addInterest <currency> <percent>");
+                        break;
+                    }
+                    return true;
+
+                case "checktxstatus":
+                    if (tokens.length < 2) {
+                        System.out.println("[BANK] [ERROR] Usage: checkTxStatus <id>");
+                        break;
+                    }
+                    return true;
+                case "sleep":
+                    if (tokens.length < 2) {
+                        System.out.println("[BANK] [ERROR] Usage: sleep <seconds>");
+                        break;
+                    }
+                    return true;
+                default:
+                    System.out.println("[BANK] [ERROR] Unknown command: " + cmd);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[BANK] [ERROR] Exception: " + e.getMessage());
+        }
+
+        return false;
     }
 
     public void executeTransaction(String input) {
@@ -32,26 +94,14 @@ public class CommandParser {
                     break;
 
                 case "getquickbalance":
-                    if (tokens.length < 2) {
-                        System.out.println("[BANK] [ERROR] Usage: getQuickBalance <currency>");
-                        break;
-                    }
                     repository.getQuickBalance(tokens[1]);
                     break;
 
                 case "getsyncedbalance":
-                    if (tokens.length < 2) {
-                        System.out.println("[BANK] [ERROR] Usage: getSyncedBalance <currency>");
-                        break;
-                    }
                     repository.getSyncedBalanceNaive(tokens[1]);
                     break;
 
                 case "deposit":
-                    if (tokens.length < 3) {
-                        System.out.println("[BANK] [ERROR] Usage: deposit <currency> <amount>");
-                        break;
-                    }
                     String depositCurrency = tokens[1];
                     double depositAmount = Double.parseDouble(tokens[2]);
                     repository.deposit(depositCurrency, depositAmount);
@@ -59,10 +109,6 @@ public class CommandParser {
                     break;
 
                 case "addinterest":
-                    if (tokens.length < 3) {
-                        System.out.println("[BANK] [ERROR] Usage: addInterest <currency> <percent>");
-                        break;
-                    }
                     String interestCurrency = tokens[1];
                     double percent = Double.parseDouble(tokens[2]);
                     repository.addInterest(interestCurrency, percent);
@@ -70,10 +116,6 @@ public class CommandParser {
                     break;
 
                 case "checktxstatus":
-                    if (tokens.length < 2) {
-                        System.out.println("[BANK] [ERROR] Usage: checkTxStatus <id>");
-                        break;
-                    }
                     repository.checkTxStatus(tokens[1]);
                     break;
 
@@ -86,11 +128,6 @@ public class CommandParser {
                     break;
 
                 case "sleep":
-                    if (tokens.length < 2) {
-                        System.out.println("[BANK] [ERROR] Usage: sleep <seconds>");
-                        break;
-                    }
-
                     double seconds = Double.parseDouble(tokens[1]);
                     repository.sleep(seconds);
                     break;
