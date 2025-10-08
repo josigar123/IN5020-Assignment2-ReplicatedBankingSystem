618e1b788b804681b26a03963b689b5bf040b59c
Stavros <stavrosw@hotmail.com>
2025-10-08 16:52:11 +0200

    comments to bankconfig

--- bank-server/src/main/java/BankConfig.java
+++ bank-server/src/main/java/BankConfig.java
@@ -1,22 +1,25 @@
 public record BankConfig(
-        String bankBindingName,
-        String mdsBindingName,
-        String accountName,
-        int numberOfReplicas,
-        String currencyFileName,
-        String transactionFileName
+        String bankBindingName,         // The name used to bind the bank server instance
+        String mdsBindingName,          // The name used to bind the message delivery service
+        String accountName,             // The name of the bank account associated with this server
+        int numberOfReplicas,           // Number of replica servers 
+        String currencyFileName,        // File containing currencies
+        String transactionFileName      // File containing predifined transactions
 ) {
+    // Creates a BankConfig instance by parsing cmd line arguments
     public static BankConfig fromArgs(String[] args) {
         if (args.length < 5 || args.length > 6) {
             throw new IllegalArgumentException("[BANK] Usage: BankServer <bankBindingName> <mdsBindingName> <accountName> <numberOfReplicas> <currencyFileName> [transactionFileName]");
         }
+
+        // Parse and create a new configuration object from the provided arguments
         return new BankConfig(
-                args[0],
-                args[1],
-                args[2],
-                Integer.parseInt(args[3]),
-                args[4],
-                args.length == 6 ? args[5] : null
+                args[0],                            //BankBindingName
+                args[1],                            //mdsBindingName
+                args[2],                            //accountName
+                Integer.parseInt(args[3]),          //numberOfReplicas
+                args[4],                            //currencyFileName
+                args.length == 6 ? args[5] : null   //transactionFileName
         );
     }
 }
